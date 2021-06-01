8f084a7e03ef14f29e4612445434d81a75f80b9d
rodrigo montemor <rkmontemor@hotmail.com>
2021-06-01 16:44:08 -0300

    CSS listagem

--- N2_2BIM/N2_2BIM/DAO/ConexaoBD.cs
+++ N2_2BIM/N2_2BIM/DAO/ConexaoBD.cs
@@ -10,8 +10,8 @@ namespace N2_2BIM.DAO
     {
         public static SqlConnection GetConexao()
         {
-            string strCon = "Data Source=LOCALHOST\\SQLEXPRESS;Initial Catalog=MoveUp;integrated security=true";
-            //string strCon = "Data Source=LAPTOP-9EADM12E; Database=MoveUp; integrated security=true";
+            //string strCon = "Data Source=LOCALHOST\\SQLEXPRESS;Initial Catalog=MoveUp;integrated security=true";
+            string strCon = "Data Source=LAPTOP-9EADM12E; Database=MoveUp; integrated security=true";
             //string strCon = "Data Source=GUSTAVO-PC;Initial Catalog=MoveUp;integrated security=true";
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
